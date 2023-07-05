a101b68d6c4f70a583e70a127782cca50fb2633a
0xhackie <138706646+0xhackie@users.noreply.github.com>
2023-07-05 22:59:54 +0530

    Initial commit

--- scoreboard/admin.py
+++ scoreboard/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+'''
+username: admin
+password: c3VwZXItc2VjcmV0LWtleQ==
+
+Ezhil, 2020-05-02 16:00:00+05:30
+password is in common base64 encoding
+'''
